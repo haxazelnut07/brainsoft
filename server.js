4123f2907ee360d31c95aa355efe5ed2ae7600a7
haxazelnut07 <syahrulsobari0911@gmail.com>
2025-03-10 10:42:49 +0700

    update

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const cors = require("cors");
 
+app.use(cors())
 app.use(express.json()); // Middleware untuk parsing JSON
 
 // Database sementara untuk menyimpan pesan terenkripsi
